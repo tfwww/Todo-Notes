06ff11522d66ee9e7277a79ebcc26d818665cef0
wmzhong <zhongwm.net@gmail.com>
2017-01-13 16:02:43 +0800

    完成 todo 条目功能

--- todo.js
+++ todo.js
@@ -3,6 +3,24 @@ var log = function() {
     console.log.apply(console, arguments);
 }
 
+var templateItem = function() {
+    var t = `
+            <div class="todo-item">
+                <h3>TODAY</h3>
+                <button type="button" name="button">+</button>
+                <h3>TOMORROW</h3>
+                <h3>UPCOMING</h3>
+                <h3>SOMEDAY</h3>
+            </div>
+    `
+    return t
+}
+
+var box = document.querySelector('.category')
+box.addEventListener('click', function(event) {
+    var template = templateItem()
+    box.insertAdjacentHTML('beforeend', template)
+})
 var __main = function() {
 
 }
