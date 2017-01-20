6ad1d7dd5f8eccdeb1c635772f17f67ed7abd6ef
wmzhong <zhongwm.net@gmail.com>
2017-01-20 18:15:15 +0800

    完善 item 添加功能

--- todo.js
+++ todo.js
@@ -21,30 +21,53 @@ var addItem = function() {
         var task = itemEle.value
         if (event.key === 'Enter' && task != '') {
             var template = templateItem(task)
-            list.insertAdjacentHTML('beforeend', template)
+            list.insertAdjacentHTML('afterend', template)
         }
     })
 }
 
 var checkTask = function() {
-    var tasks = document.querySelectorAll('label')
-    var checkBoxes = document.querySelectorAll('input')
+    // 这种写法如果后续又有 input 添加，无法对后续的 input 生效
+    // 醒悟到这里应该用事件委托
 
-    for (var i = 0; i < checkBoxes.length; i++) {
-        var item = checkBoxes[i]
-        var task = tasks[i]
-        log('task', task)
-        item.addEventListener('change', function(event) {
-            log('change')
-            for (var i = 0; i < tasks.length; i++) {
-                var task = tasks[i]
-                if (checkBoxes[i].checked) {
-                    task.classList.add('task-done')
-                } else {
-                    task.classList.remove('task-done')
-                }
-            }
-        })
+    // var tasks = document.querySelectorAll('label')
+    // var checkBoxes = document.querySelectorAll('input')
+    // for (var i = 0; i < checkBoxes.length; i++) {
+    //     var item = checkBoxes[i]
+    //     var task = tasks[i]
+    //     log('item', item)
+    //     item.addEventListener('change', function(event) {
+    //         log('change')
+    //         for (var i = 0; i < tasks.length; i++) {
+    //             var task = tasks[i]
+    //             if (checkBoxes[i].checked) {
+    //                 task.classList.add('task-done')
+    //             } else {
+    //                 task.classList.remove('task-done')
+    //             }
+    //         }
+    //     })
+    // }
+
+    // 事件委托的写法
+    var list = document.querySelector('ul')
+    list.addEventListener('change', function(event) {
+        var target = event.target
+        // log('target', event)
+        if (target.type === 'checkbox') {
+            // log('in target', target)
+            var label = target.nextSibling
+            // log('label', label)
+            toggleClass(label, 'task-done')
+        }
+    })
+}
+
+var toggleClass = function(ele, className) {
+    if (ele.classList.contains(className)) {
+        ele.classList.remove(className)
+    } else {
+        ele.classList.add(className)
     }
 }
 
