c4bb3f43626f33a141ffaca487e182c6867a63a6
wmzhong <zhongwm.net@gmail.com>
2017-01-20 15:58:57 +0800

    添加“点击添加条目”功能

--- todo.js
+++ todo.js
@@ -3,57 +3,30 @@ var log = function() {
     console.log.apply(console, arguments);
 }
 
-var templateBox = function() {
-    var t = `
-            <div class="todo-item">
-                <h3>TODAY</h3>
-                <button class="add-button" type="button" name="button">+</button>
-                <h3>TOMORROW</h3>
-                <h3>UPCOMING</h3>
-                <h3>SOMEDAY</h3>
-            </div>
-    `
-    return t
-}
-
-var templateItem = function() {
+var templateItem = function(item) {
     var item = `
-                <div class="checkoff-item">
-                    <a href="#link" class="checkoff-link todo-item">
-                        <img src="plus-icon.png">
-                    </a>
-                </div>
+                <li>
+                    <input type="checkbox" name="" value=""><label>${item}</label>
+                </li>
     `
     return item
 }
 
-var showDetails = function() {
-    var box = document.querySelector('.category')
-    box.addEventListener('click', function(event) {
-        var target = event.target
-        if (target.classList.contains('category')) {
-            var template = templateBox()
-            box.insertAdjacentHTML('beforeend', template)
-        }
-    })
-}
-
 var addItem = function() {
-    var body = document.querySelector('body')
-    body.addEventListener('click', function(event) {
-        var button = event.target
-        log('event target', button)
-        if (button.classList.contains('add-button')) {
-            var template = templateItem()
-            var hideEle = document.querySelector('.todo-item')
-            body.insertAdjacentHTML('beforeend', template)
-            hideEle.classList.add('hide')
+    var list = document.querySelector('.todo-title')
+    var itemEle = document.querySelector('#id-input-add')
+    itemEle.addEventListener('keydown', function(event) {
+        var key = event.key
+        log('event key', key)
+        var task = itemEle.value
+        if (event.key === 'Enter') {
+            var template = templateItem(task)
+            list.insertAdjacentHTML('beforeend', template)
         }
     })
 }
 
 var __main = function() {
-    showDetails()
     addItem()
 }
 
